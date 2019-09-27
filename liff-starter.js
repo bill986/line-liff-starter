3be29f283099b82cccc9ef5b238dbb54166a2311
Bill Kuo <47585770+bill986@users.noreply.github.com>
2019-09-27 15:54:34 +0800

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -65,24 +65,8 @@ function initializeApp(data) {
 
     //get profile call
     document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-            document.getElementById('displaynamefield').textContent = profile.displayName;
-
-            var profilePictureDiv = document.getElementById('profilepicturediv');
-            if (profilePictureDiv.firstElementChild) {
-                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-            }
-            var img = document.createElement('img');
-            img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
-            profilePictureDiv.appendChild(img);
-
-            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
+        document.getElementById('code').textContent = "text";
+        toggleProfileData();
     });
 }
 
