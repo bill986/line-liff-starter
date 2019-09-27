344689b96699ff158b0a6a5081a5f9ed5bdfdcd9
Bill Kuo <47585770+bill986@users.noreply.github.com>
2019-09-27 12:18:12 +0800

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -29,10 +29,6 @@ function initializeApp(data) {
         liff.sendMessages([{
             type: 'text',
             text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
         }]).then(function () {
             window.alert("Message sent");
         }).catch(function (error) {
